5fbb3475f08efc891978e415a27f1caf305077d9
Megan Horsfield <meganthadev@gmail.com>
2020-03-18 06:26:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,15 @@ class Student
     first_student
   end
   
-  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
    def self.all_students_in_grade_X(grade_num)
     sql = <<-SQL
