2dc49dad13a480c3ddc3d51acf611d7ec64914aa
Megan Horsfield <meganthadev@gmail.com>
2020-03-18 06:28:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,19 +42,17 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.students_below_12th_grade
+   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT * FROM students WHERE grade <= 11
+      SELECT *
+      FROM students
+      WHERE grade < 12
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-      end
-  
-   DB[:conn].execute(sql, name).map do |row|
-     self.new_from_db(row)
-    end.first 
-   end  
+    end
+  end
    
   def self.first_X_students_in_grade_10(num_of_students)
     sql = <<-SQL
