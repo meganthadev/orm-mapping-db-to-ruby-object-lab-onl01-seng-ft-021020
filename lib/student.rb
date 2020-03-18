82a569f10ee9ca9090a6f1f3093d0e0d111d63b4
Megan Horsfield <meganthadev@gmail.com>
2020-03-18 06:23:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,8 +49,7 @@ class Student
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
-  end
+      end
   
    DB[:conn].execute(sql, name).map do |row|
      self.new_from_db(row)
